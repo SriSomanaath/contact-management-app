cb8f87db20473fc10167eb34f5af88f8e69e4e9f
SriSomanaath <govindalapudisrinath@gmail.com>
2023-07-08 18:13:59 +0530

    user registeration

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,23 +1,17 @@
 const express = require("express");
-// const {
-//   registerUser,
-//   currentUser,
-//   loginUser,
-// } = require("../controllers/userController");
+const {
+  registerUser,
+  currentUser,
+  loginUser,
+} = require("../controllers/userController");
 // const validateToken = require("../middleware/validateTokenHandler");
 
 const router = express.Router();
 
-router.post("/register", (req,res)=>{
-    res.json({message:"Register the user"})
-});
+router.post("/register", registerUser);
 
-router.post("/login", (req,res)=>{
-    res.json({message:"login user"})
-});
+router.post("/login", loginUser);
 
-router.get("/current", (req,res)=>{
-    res.json({message:"Current the information"})
-});
+router.get("/current", currentUser);
 
 module.exports = router;
