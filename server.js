8b7b74a3fcf22ae81edaebc93f903385410a16e5
SriSomanaath <govindalapudisrinath@gmail.com>
2023-07-08 09:15:10 +0530

    add userModel and mongoose connection established

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorhandler");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+connectDb();
 
 app.use(express.json())
 app.use("/api/contacts",require("./routes/contactRoutes"));
